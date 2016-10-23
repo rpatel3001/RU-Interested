0c71dcba3461a95c1ece7f2b2fb87fc4176f5fcc
Rajan Patel <rpatel3001@gmail.com>
2016-10-23 17:17:33 -0400

    better filtering

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # RU-Interested
+Got a long wait between classes? See if there's an interesting lecture going on near you!
--- serve.py
+++ serve.py
@@ -23,14 +23,17 @@ print("Class lists loaded")
 def submit():
 	time = int(datetime.now().strftime("%H%M"))
 	day = daysOfWeek[int(datetime.now().strftime("%w"))]
+	day = "W"
 	form = SpecifierForm()
 	classes = []
 	if form.campus.data != "None":
-		classes = [x for x in classesIn[form.campus.data] if x["time"] > time and x["day"] == "M"]																		
-	form.building.choices = [(x["code"], x["name"]) for x in dataIn["buildings"] if x["campus"] == form.campus.data and x["code"] in [y["building"] for y in [w for z in classesIn for w in classesIn[z]]]]
-	print(form.building.choices)
+		classes = [x for x in classesIn[form.campus.data] if x["time"] > time and x["day"] == day and (form.building.data == [] or x["building"] in form.building.data)]
+	print(form.building.data)
+	classBuildings = [x["building"] for x in classes]
+	form.building.choices = [(x["code"], x["name"]) for x in dataIn["buildings"] if x["campus"] == form.campus.data and x["code"] in classBuildings]
 	return render_template("main.html", form=form, results=classes)
 
 class SpecifierForm(FlaskForm):
 	campus = SelectField('campus', choices=[(1, "College Avenue"), (2, "Busch"), (3, "Livingston"), (4, "Cook/Douglas")])
 	building = SelectMultipleField("building")
+	
